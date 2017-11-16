3f305b5d131b31706e257fa2bde67888632a39aa
Marco <marco98x2@gmail.com>
2017-11-16 16:09:57 +0000

    created comments page

--- app.js
+++ app.js
@@ -2,33 +2,16 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    Campground = require("./models/campground"),
-    seedDB = require("./seeds");
-
-seedDB();
-mongoose.connect("mongodb://localhost/yelp_camp");
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    seedDB      = require("./seeds");
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
-
-
-// Campground.create(
-//      {
-//          name: "Granite Hill", 
-//          image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-//          description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
-         
-//      },
-//      function(err, campground){
-//       if(err){
-//           console.log(err);
-//       } else {
-//           console.log("NEWLY CREATED CAMPGROUND: ");
-//           console.log(campground);
-//       }
-//     });
-
-    
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -40,7 +23,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-          res.render("index",{campgrounds:allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds});
        }
     });
 });
@@ -65,21 +48,54 @@ app.post("/campgrounds", function(req, res){
 
 //NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         } else {
+            console.log(foundCampground);
             //render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
-})
+});
+
+//COMMENTS ROUTES
+
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    Campground.findById(req.params.id, function(err,campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground:campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err,comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+ campground._id);
+                }
+            });
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
--- seeds.js
+++ seeds.js
@@ -1,19 +1,19 @@
 var mongoose = require("mongoose");
 var Campground = require ("./models/campground");
-var Comment = require(".models/comment");
+var Comment = require("./models/comment");
 
 var data = [
     {name:"Cloud's Rest",
     image:"https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg",
-    description:"blah blah blah"
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel tincidunt mi, id congue augue. Sed rutrum scelerisque leo, luctus placerat mi malesuada sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi sit amet metus vitae nisi maximus bibendum sit amet eu nunc. In laoreet nisi ut nulla fermentum suscipit. Etiam ac molestie diam. Aenean vitae lacus ac purus ullamcorper placerat efficitur fermentum massa. Nullam eget metus nulla. In et consectetur libero. quis lorem dignissim ex sodales malesuada. Pellentesque enim magna, hendrerit posuere purus quis, consectetur euismod lorem. Vestibulum sed ipsum nisl. Maecenas vulputate condimentum erat a rutrum. Etiam eu tincidunt libero. Sed maximus in nisi eu viverra. Donec pretium commodo faucibus. Vivamus id ultricies dolor, a mattis elit. Nullam turpis magna, pharetra vehicula maximus sit amet, elementum sodales augue. Morbi consectetur augue et turpis tincidunt, non imperdiet magna suscipit. Donec leo magna, laoreet ac pellentesque at, pellentesque in nulla. Aenean vulputate euismod placerat. Cras sapien neque, sagittis convallis diam nec, elementum luctus lacus."
     },
     {name:"Desert Mesa",
     image:"https://farm3.staticflickr.com/2353/2069978635_2eb8b33cd4.jpg",
-    description:"blah blah blah"
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel tincidunt mi, id congue augue. Sed rutrum scelerisque leo, luctus placerat mi malesuada sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi sit amet metus vitae nisi maximus bibendum sit amet eu nunc. In laoreet nisi ut nulla fermentum suscipit. Etiam ac molestie diam. Aenean vitae lacus ac purus ullamcorper placerat efficitur fermentum massa. Nullam eget metus nulla. In et consectetur libero.Donec quis lorem dignissim ex sodales malesuada. Pellentesque enim magna, hendrerit posuere purus quis, consectetur euismod lorem. Vestibulum sed ipsum nisl. Maecenas vulputate condimentum erat a rutrum. Etiam eu tincidunt libero. Sed maximus in nisi eu viverra. Donec pretium commodo faucibus. Vivamus id ultricies dolor, a mattis elit. Nullam turpis magna, pharetra vehicula maximus sit amet, elementum sodales augue. Morbi consectetur augue et turpis tincidunt, non imperdiet magna suscipit. Donec leo magna, laoreet ac pellentesque at, pellentesque in nulla. Aenean vulputate euismod placerat. Cras sapien neque, sagittis convallis diam nec, elementum luctus lacus."
     },
     {name:"Canion Floor",
     image:"https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg",
-    description:"blah blah blah"
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel tincidunt mi, id congue augue. Sed rutrum scelerisque leo, luctus placerat mi malesuada sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi sit amet metus vitae nisi maximus bibendum sit amet eu nunc. In laoreet nisi ut nulla fermentum suscipit. Etiam ac molestie diam. Aenean vitae lacus ac purus ullamcorper placerat efficitur fermentum massa. Nullam eget metus nulla. In et consectetur libero.Donec quis lorem dignissim ex sodales malesuada. Pellentesque enim magna, hendrerit posuere purus quis, consectetur euismod lorem. Vestibulum sed ipsum nisl. Maecenas vulputate condimentum erat a rutrum. Etiam eu tincidunt libero. Sed maximus in nisi eu viverra. Donec pretium commodo faucibus. Vivamus id ultricies dolor, a mattis elit. Nullam turpis magna, pharetra vehicula maximus sit amet, elementum sodales augue. Morbi consectetur augue et turpis tincidunt, non imperdiet magna suscipit. Donec leo magna, laoreet ac pellentesque at, pellentesque in nulla. Aenean vulputate euismod placerat. Cras sapien neque, sagittis convallis diam nec, elementum luctus lacus."
     }
     ];
 
@@ -33,9 +33,11 @@ function seedDB() {
          } else {
              console.log("added a campground");
              //create a comment
-             Comment.create({text:"This place was great, but I wish there was internet",
-                            author:"Homer"
-             }), function(err, comment){
+             Comment.create(
+                 {
+                     text:"This place was great, but I wish there was internet",
+                     author:"Homer"
+             }, function(err, comment){
                  if(err){
                      console.log(err);
                  } else {
@@ -43,7 +45,7 @@ function seedDB() {
                  campground.save();
                  console.log("created new comment");
                  }
-             };
+             });
          }
      });   
     });
